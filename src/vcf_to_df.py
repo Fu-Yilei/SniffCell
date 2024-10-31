efd010934a6b9ecf27ba7892d95f01ab18cd1fb4
Fu-Yilei <fuyilei96@outlook.com>
2024-10-31 16:38:18 -0500

    fix some pylint stuff..but too many to fix so I removed pyliny workflow :)

--- src/vcf_to_df.py
+++ src/vcf_to_df.py
@@ -1,10 +1,17 @@
+
 import pandas as pd
 import numpy as np
-import re
 
 def read_vcf_to_df(vcf_file):
+    """_summary_
+
+    Args:
+        vcf_file (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     records = []
-    
     for record in vcf_file.fetch():
         if record.info["SVTYPE"] in ["INS", "DEL"]:
             df_record = {
@@ -21,17 +28,27 @@ def read_vcf_to_df(vcf_file):
             sv_len = df_record["sv_len"] if record.info["SVTYPE"] == "DEL" else 0
             stdev_len = df_record["stdev_len"] 
             ref_start = df_record["location"] - stdev_pos if stdev_pos != "NA" else np.nan
-            ref_end = df_record["location"] + stdev_pos + stdev_len - sv_len if stdev_pos != "NA" else np.nan
+            if stdev_pos != "NA":
+                ref_end = df_record["location"] + stdev_pos + stdev_len - sv_len
+            else:
+                ref_end = np.nan
 
             df_record.update({"ref_start": int(ref_start), "ref_end": int(ref_end)})
             records.append(df_record)
-    
     sv_df = pd.DataFrame(records, columns=["chr", "location", "id", "sv_len", "supporting_reads", "stdev_len", "stdev_pos", "ref_start", "ref_end"])
     return sv_df
 
 
 
 def read_vcf_to_df_comp(vcf_file):
+    """_summary_
+
+    Args:
+        vcf_file (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     records = []
     sample_list = ["HG002", "HG00438", "HG005", "HG02257", "HG02486", "HG02622"]
     for record in vcf_file.fetch():
