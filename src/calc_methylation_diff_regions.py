6828458aa073b59d074baea410d167179cf44548
Fu-Yilei <fuyilei96@outlook.com>
2024-11-04 10:22:48 -0600

    added multiprocessing support

--- src/calc_methylation_diff_regions.py
+++ src/calc_methylation_diff_regions.py
@@ -5,6 +5,7 @@ from src.vcf_to_df import read_vcf_to_df
 from src.get_base_modification_dictionary import get_base_modification_dictionary_basic_supporting_reads, get_base_modification_dictionary_new_bam
 from src.statistic_tests import calculate_ttest, calculate_ranksum, calculate_fisher
 from tqdm import tqdm
+from multiprocessing import Pool
 
 
 def filter_dict_with_sv(input_dict, sv_start, sv_end):
@@ -26,89 +27,106 @@ def filter_dict_with_sv(input_dict, sv_start, sv_end):
     return output_dict
 
 
+def process_individual_sv(sv, hapmap_ref_file, referece_sequence, output_bam_folder, output_bam, sv_discovery_range, smoothing, test_function):
+    modification_dict = get_base_modification_dictionary_new_bam(bam_file=hapmap_ref_file, 
+                                                             ref_seq=referece_sequence, 
+                                                             chromosome=sv.chr, 
+                                                             phase_region=(int(sv.ref_start)-sv_discovery_range, int(sv.ref_end)+sv_discovery_range), 
+                                                             sv_supporting_reads=sv.supporting_reads, sv_id=sv.id, output_bam_folder=output_bam_folder, output_bam=output_bam)
+    modification_dict = src.smoothing.smoothing_comp(modification_dict, key_diff_threshold=smoothing)
+    if test_function == 'ttest':
+        ranksum_dict = calculate_ttest(modification_dict)
+    elif test_function == 'ranksum':
+        ranksum_dict = calculate_ranksum(modification_dict)
+    elif test_function == 'fisher':
+        ranksum_dict = calculate_fisher(modification_dict)
+    else:
+        ranksum_dict = calculate_ttest(modification_dict)
+
+    # ranksum_dict = calculate_ttest(modification_dict) # nan if list length is 0
+    ranksum_dict = filter_dict_with_sv(ranksum_dict, int(sv.ref_start), int(sv.ref_end))
+    cpg_diff_locs = {i: p_value for i, p_value in ranksum_dict.items() if p_value < 0.05}
+    cpg_same_locs = {i: p_value for i, p_value in ranksum_dict.items() if p_value > 0.05}
+    # Calculate the percentage of different CpG locations
+    if len(cpg_diff_locs) + len(cpg_same_locs) > 0:
+        cpg_diff_percentage = len(cpg_diff_locs) / (len(cpg_diff_locs) + len(cpg_same_locs))
+    else:
+        cpg_diff_percentage = None  # or some default value
+
+    return cpg_diff_percentage
+
+def process_sv_wrapper(args):
+    return process_individual_sv(*args)
+
 def calculate_methylation_diff_region_bam(sv_vcf, input_bam, reference_genome, 
-                                          output_bam_folder, smoothing, output_bam=True, 
-                                          min_supporting_read_num = 5, 
+                                          output_bam_folder, smoothing, threads, output_bam=True, 
+                                          min_supporting_read_num=5, 
                                           sv_discovery_range=1000,
-                                            test_function = 'ttest'):
+                                          test_function='ttest'):
     
     hapmap_ref_file = pysam.AlignmentFile(input_bam)
     referece_sequence = pysam.Fastafile(reference_genome)
     filtered_mosaic_sv = pysam.VariantFile(sv_vcf)
     filtered_mosaic_sv_df = read_vcf_to_df(filtered_mosaic_sv)
-    cpg_diff_percentages = []
-    filtered_mosaic_sv_df_filtered = filtered_mosaic_sv_df[filtered_mosaic_sv_df.supporting_reads.apply(len)>min_supporting_read_num]
-    for _, test_sv in tqdm(filtered_mosaic_sv_df_filtered.iterrows(), total=len(filtered_mosaic_sv_df_filtered), desc="Processing SVs"):
-        modification_dict = get_base_modification_dictionary_new_bam(bam_file=hapmap_ref_file, 
-                                                             ref_seq=referece_sequence, 
-                                                             chromosome=test_sv.chr, 
-                                                             phase_region=(int(test_sv.ref_start)-sv_discovery_range, int(test_sv.ref_end)+sv_discovery_range), 
-                                                             sv_supporting_reads=test_sv.supporting_reads, sv_id=test_sv.id, output_bam_folder=output_bam_folder, output_bam=output_bam)
-        modification_dict = src.smoothing.smoothing_comp(modification_dict, key_diff_threshold=smoothing)
-        if test_function == 'ttest':
-            ranksum_dict = calculate_ttest(modification_dict)
-        elif test_function == 'ranksum':
-            ranksum_dict = calculate_ranksum(modification_dict)
-        elif test_function == 'fisher':
-            ranksum_dict = calculate_fisher(modification_dict)
-        else:
-            ranksum_dict = calculate_ttest(modification_dict)
+    filtered_mosaic_sv_df_filtered = filtered_mosaic_sv_df[filtered_mosaic_sv_df.supporting_reads.apply(len) > min_supporting_read_num]
 
-        # ranksum_dict = calculate_ttest(modification_dict) # nan if list length is 0
-        ranksum_dict = filter_dict_with_sv(ranksum_dict, int(test_sv.ref_start), int(test_sv.ref_end))
-        cpg_diff_locs = {i: p_value for i, p_value in ranksum_dict.items() if p_value < 0.05}
-        cpg_same_locs = {i: p_value for i, p_value in ranksum_dict.items() if p_value > 0.05}
-        # Calculate the percentage of different CpG locations
-        if len(cpg_diff_locs) + len(cpg_same_locs) > 0:
-            cpg_diff_percentage = len(cpg_diff_locs) / (len(cpg_diff_locs) + len(cpg_same_locs))
-        else:
-            cpg_diff_percentage = None  # or some default value
-        
-        cpg_diff_percentages.append(cpg_diff_percentage)    
+    args_list = [(test_sv, hapmap_ref_file, referece_sequence, output_bam_folder, output_bam, sv_discovery_range, smoothing, test_function) 
+                 for _, test_sv in filtered_mosaic_sv_df_filtered.iterrows()]
+
+    with Pool(threads) as pool:
+        cpg_diff_percentages = list(tqdm(pool.imap(process_sv_wrapper, args_list), total=len(args_list), desc="Processing SVs"))
+
     filtered_mosaic_sv_df_filtered['cpg_diff_percentage'] = cpg_diff_percentages
 
     return filtered_mosaic_sv_df_filtered
 
 
-def calculate_methylation_diff_region_benchmark(input_bam, input_vcf, reference_genome, output, sv_discovery_interval, benchmark_second_bam, min_supporting_reads, test_function, smoothing):
-    cpg_diff_percentages = []
+def process_individual_sv_benchmark(sv, hapmap_ref_file, referece_sequence, output_bam_folder, output_bam, sv_discovery_range, benchmark_second_bam, smoothing, test_function):
+    modification_dict = get_base_modification_dictionary_new_bam(bam_file=hapmap_ref_file, 
+                                                             ref_seq=referece_sequence, 
+                                                             chromosome=sv.chr, 
+                                                             phase_region=(int(sv.ref_start)-sv_discovery_range, int(sv.ref_end)+sv_discovery_range), 
+                                                             sv_supporting_reads=sv.supporting_reads, sv_id=sv.id, output_bam_folder=output_bam_folder, output_bam=output_bam)
+    modification_dict = src.smoothing.smoothing_comp(modification_dict, key_diff_threshold=smoothing)
+    benchmark_second_bam_sv_methylation_dict = get_base_modification_dictionary_new_bam(bam_file=benchmark_second_bam, 
+                                                             ref_seq=referece_sequence, 
+                                                             chromosome=sv.chr, 
+                                                             phase_region=(int(sv.ref_start)-sv_discovery_range, int(sv.ref_end)+sv_discovery_range), 
+                                                             sv_supporting_reads=None, sv_id=sv.id, output_bam_folder=output_bam_folder, output_bam=output_bam)
+    benchmark_second_bam_sv_methylation_dict = src.smoothing.smoothing_comp(benchmark_second_bam_sv_methylation_dict, key_diff_threshold=smoothing)
+    if test_function == 'ttest':
+        ranksum_dict = calculate_ttest(modification_dict, benchmark_second_bam_sv_methylation_dict)
+    elif test_function == 'ranksum':
+        ranksum_dict = calculate_ranksum(modification_dict, benchmark_second_bam_sv_methylation_dict)
+    elif test_function == 'fisher':
+        ranksum_dict = calculate_fisher(modification_dict, benchmark_second_bam_sv_methylation_dict)
+    else:
+        ranksum_dict = calculate_ttest(modification_dict, benchmark_second_bam_sv_methylation_dict)
 
+    # ranksum_dict = calculate_ttest(modification_dict) # nan if list length is 0
+    ranksum_dict = filter_dict_with_sv(ranksum_dict, int(sv.ref_start), int(sv.ref_end))
+    cpg_diff_locs = {i: p_value for i, p_value in ranksum_dict.items() if p_value < 0.05}
+    cpg_same_locs = {i: p_value for i, p_value in ranksum_dict.items() if p_value > 0.05}
+    # Calculate the percentage of different CpG locations
+    if len(cpg_diff_locs) + len(cpg_same_locs) > 0:
+        cpg_diff_percentage = len(cpg_diff_locs) / (len(cpg_diff_locs) + len(cpg_same_locs))
+    else:
+        cpg_diff_percentage = None  
+    return cpg_diff_percentage
+
+
+def calculate_methylation_diff_region_benchmark(input_bam, input_vcf, reference_genome, output, sv_discovery_interval, benchmark_second_bam, min_supporting_reads, test_function, smoothing, threads):
     benchmarking_vcf = read_vcf_to_df(input_vcf)
-    benchmarking_vcf = benchmarking_vcf[benchmarking_vcf.supporting_reads.apply(len)>min_supporting_reads]
+    benchmarking_vcf = benchmarking_vcf[benchmarking_vcf.supporting_reads.apply(len) > min_supporting_reads]
 
-    for _, test_sv in tqdm(benchmarking_vcf.iterrows(), total=len(benchmarking_vcf), desc="Processing SVs"):
-        input_bam_sv_methylation_dict = get_base_modification_dictionary_basic_supporting_reads(
-                bam_file=input_bam, 
-                ref_seq=reference_genome, 
-                chromosome=test_sv.chr, 
-                phase_region=(int(test_sv.ref_start)-sv_discovery_interval, int(test_sv.ref_end)+sv_discovery_interval), 
-                sv_supporting_reads=test_sv.supporting_reads, sv_id=test_sv.id, output_bam_folder=output)
-        benchmark_second_bam_sv_methylation_dict = get_base_modification_dictionary_basic_supporting_reads(
-                bam_file=benchmark_second_bam, 
-                ref_seq=reference_genome, 
-                chromosome=test_sv.chr, 
-                phase_region=(int(test_sv.ref_start)-sv_discovery_interval, int(test_sv.ref_end)+sv_discovery_interval), 
-                sv_supporting_reads=None, sv_id=test_sv.id, output_bam_folder=output)
-        input_bam_sv_methylation_dict = src.smoothing.smoothing_basic(input_bam_sv_methylation_dict, key_diff_threshold=smoothing)
-        benchmark_second_bam_sv_methylation_dict = src.smoothing.smoothing_basic(benchmark_second_bam_sv_methylation_dict, key_diff_threshold=smoothing)
-        
-        if test_function == 'ttest':
-            ranksum_dict = src.statistic_tests.calculate_ttest(input_bam_sv_methylation_dict, benchmark_second_bam_sv_methylation_dict)
-        elif test_function == 'ranksum':
-            ranksum_dict = src.statistic_tests.calculate_ranksum(input_bam_sv_methylation_dict, benchmark_second_bam_sv_methylation_dict)
-        elif test_function == 'fisher':
-            ranksum_dict = src.statistic_tests.calculate_fisher(input_bam_sv_methylation_dict, benchmark_second_bam_sv_methylation_dict)
-    
-        ranksum_dict = src.calc_methylation_diff_regions.filter_dict_with_sv(ranksum_dict, int(test_sv.ref_start), int(test_sv.ref_end))
-        cpg_diff_locs = {i: p_value for i, p_value in ranksum_dict.items() if p_value < 0.05}
-        cpg_same_locs = {i: p_value for i, p_value in ranksum_dict.items() if p_value > 0.05}
-             # Calculate the percentage of different CpG locations
-        if len(cpg_diff_locs) + len(cpg_same_locs) > 0:
-            cpg_diff_percentage = len(cpg_diff_locs) / (len(cpg_diff_locs) + len(cpg_same_locs))
-        else:
-            cpg_diff_percentage = None  # or some default value
-            
-        cpg_diff_percentages.append(cpg_diff_percentage)
+    args_list = [(test_sv, input_bam, reference_genome, output, benchmark_second_bam, sv_discovery_interval, smoothing, test_function) 
+                 for _, test_sv in benchmarking_vcf.iterrows()]
+
+    with Pool(threads) as pool:
+        cpg_diff_percentages = list(tqdm(pool.imap(process_sv_wrapper_benchmark, args_list), total=len(args_list), desc="Processing SVs"))
 
     benchmarking_vcf['cpg_diff_percentage'] = cpg_diff_percentages
-    return benchmarking_vcf       
+    return benchmarking_vcf
+
+def process_sv_wrapper_benchmark(args):
+    return process_individual_sv_benchmark(*args)
--- src/main.py
+++ src/main.py
@@ -22,17 +22,18 @@ def main(argv):
     
     if benchmark_second_bam:
         benchmarking_sv_df = src.calc_methylation_diff_regions.calculate_methylation_diff_region_benchmark(input_bam, 
-                                                                                                           input_vcf, reference_genome, 
-                                                                                                           output, sv_discovery_interval, 
-                                                                                                           benchmark_second_bam, min_supporting_reads, 
-                                                                                                           test_function,smoothing=smoothing)
+                                                           input_vcf, reference_genome, 
+                                                           output, sv_discovery_interval, 
+                                                           benchmark_second_bam, min_supporting_reads, 
+                                                           test_function, smoothing=smoothing, 
+                                                           threads=threads)
         benchmarking_sv_df.to_csv(os.path.join(output, "benchmarking_sv_df.csv"))
 
     else:
         sv_methylation_df = src.calc_methylation_diff_regions.calculate_methylation_diff_region_bam(sv_vcf=input_vcf, input_bam=input_bam, 
-                                                                                reference_genome=reference_genome, output_bam_folder=output, smoothing=smoothing, 
-                                                                                output_bam=output_bam, min_supporting_read_num = min_supporting_reads, 
-                                                                                sv_discovery_range=sv_discovery_interval, test_function=test_function)
+                                            reference_genome=reference_genome, output_bam_folder=output, smoothing=smoothing, 
+                                            output_bam=output_bam, min_supporting_read_num = min_supporting_reads, 
+                                            sv_discovery_range=sv_discovery_interval, test_function=test_function, threads=threads)
         sv_methylation_df.to_csv(os.path.join(output, "sv_methylation_df.csv"))
 
 
